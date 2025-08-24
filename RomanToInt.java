6c47a631960e9854db551c2e708b3c06b9f6b22f
Mario <500honden@gmail.com>
2025-08-24 21:15:19 +0300

    Add files via upload

---
+++ RomanToInt.java
@@ -0,0 +1,109 @@
+public class RomanToInt {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.romanToInt("MCMXCIV - Expected 1994"));
+        System.out.println(sol.romanToInt("III - Expected 3" ));
+        System.out.println(sol.romanToInt("LVIII - Expected 58"));
+        System.out.println(sol.romanToInt("IV - Expected 4"));
+    }
+}
+
+
+class Solution {
+    public int romanToInt(String s) {
+        
+        int I = 1;
+        int V = 5;
+        int X = 10;
+        int L = 50;
+        int C = 100;
+        int D = 500;
+        int M = 1000;
+        int result = 0;
+
+
+
+        for(int i = 0; i < s.length() ; i++){
+
+
+            int Roman = s.charAt(i);
+
+            int Number = 0;
+
+            switch(Roman){
+
+                case 'I':
+                Number = 1;
+                break;
+
+                case 'V':
+                Number =5;
+                break;
+
+                case 'X':
+                Number =10;
+                break;
+
+                case 'L':
+                Number = 50;
+                break;
+
+                case 'C':
+                Number = 100;
+                break;
+
+                case 'D':
+                Number = 500;
+                break;
+
+                case 'M':
+                Number = 1000;
+                break;
+
+            }
+
+
+
+
+            if (i > 0 ){
+
+
+                    if(s.charAt(i-1) == 'I' && (s.charAt(i) == 'V' || s.charAt(i) == 'X')) {
+
+
+                    result -= 2;
+
+
+                    }
+
+                    if(s.charAt(i-1) == 'X' && (s.charAt(i) == 'L' || s.charAt(i) == 'C')) {
+
+
+                        result -= 20;
+
+
+                    }
+
+
+                    if(s.charAt(i-1) == 'C' && (s.charAt(i) == 'D' || s.charAt(i) == 'M')) {
+
+
+                        result -= 200;
+
+
+                }
+
+
+            }
+
+
+            result += Number;
+
+
+        }
+
+        
+    return result;
+
+    }
+}
